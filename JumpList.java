46c6d23a10d54e2a78ea9d2c64e8c3c38a8aac40
CCBB <alex@ubuntu.(none)>
2013-04-14 16:58:18 -0400

    Add draft jumplist delete() method. Need bug fixing.

--- JumpList.java
+++ JumpList.java
@@ -30,7 +30,9 @@ public class JumpList  implements OrderedSet {
 		int oList[] = new int[10];
 		for (int i = oList.length-1; i >= 0; i--)
 		{
-			final int randomNum = (int) (Math.random() * 100);
+			int randomNum = (int) (Math.random() * 100);
+			while (Arrays.binarySearch(oList, randomNum) >= 0)
+				randomNum = (int) (Math.random() * 100);
 			oList[i] = randomNum;
 		}
 		Arrays.sort(oList);
@@ -77,13 +79,6 @@ public class JumpList  implements OrderedSet {
 			currNode = currNode.nextNode;
 		}
 	}
-
-	/*public void build_jumplist(int headIndex, int tailIndex){
-		int numOfItems = tailIndex - headIndex + 1;
-
-		if (numOfItems == 1)
-			return;
-	}*/
 
 	public JumpListNode build_perfect_jumplist(JumpListNode x, int n){
 		while (n > 1)
@@ -191,7 +186,81 @@ public class JumpList  implements OrderedSet {
 
 	public void delete(Comparable data)
 	{
+		if (!this.contains(data))
+		{
+			System.out.println("Deleted Item: " + data + " is not in the list.");
+			return;
+		}
 
+		JumpListNode x = this.headNode;
+		JumpListNode s = new JumpListNode();
+		while (x.value.compareTo(data) != 0)
+		{
+			s = x;
+
+			double alpha = 0.40;	//0 < alpha < 0.5; alpha could be 1 - 1/sqrt(2)
+			double temp = (double)(x.ncount + 1)/(2 + x.ncount + x.jcount);
+			if ((1 - alpha) < temp && temp < alpha){
+				this.build_perfect_jumplist(x, (1 + x.ncount + x.jcount));
+			}
+			if (x.jumpToNode.value.compareTo(data) <= 0)
+			{
+				x.jcount--;
+				x = x.jumpToNode;
+			}
+			else if (x.nextNode.value.compareTo(data) <= 0)
+			{
+				x.ncount--;
+				x = x.nextNode;
+			}
+		}
+		if (s.jumpToNode == x)
+		{
+			if (x.jumpToNode == x)
+			{
+				if (s.nextNode == x)
+					s.jumpToNode = s;
+				else
+					s.jumpToNode = s.nextNode;
+
+				s.jcount = s.ncount;
+				s.ncount = 0;
+			}
+			else
+				s.jumpToNode = x.nextNode;
+
+			if (s.nextNode != x)
+			{
+				s = s.nextNode;
+				while (s.jumpToNode != s)
+					s = s.jumpToNode;
+			}
+
+			s.nextNode = x.nextNode;
+			if (x.ncount != 0)
+			{
+				JumpListNode y = x.nextNode;
+				JumpListNode t1 = x.jumpToNode;
+				int jc1 = x.jcount;
+
+				while (y.ncount != 0)
+				{
+					JumpListNode t2 = y.jumpToNode;
+					int jc2 = y.jcount;
+
+					y.jumpToNode = t1;
+					y.ncount = y.ncount + jc2;
+					y.jcount = jc1;
+					t1 = t2;
+					jc1 = jc2;
+					y = y.nextNode;
+				}
+				y.jumpToNode = t1;
+				y.ncount = y.jcount;
+				y.jcount = jc1;
+			}
+			x = null;
+		}
 	}
 
 	public void successor(Comparable data)
--- JumpListNode.java
+++ JumpListNode.java
@@ -2,16 +2,21 @@ public class JumpListNode {
 	Comparable value;
 	JumpListNode nextNode;
 	JumpListNode jumpToNode;
-	boolean isHead;
-	boolean isTail;
+	/*boolean isHead;
+	boolean isTail;*/
 	int ncount;
 	int jcount;
 
 	public JumpListNode(Comparable value){
 		this.value = value;
-		this.isHead = false;
-		this.isTail = false;
+		/*this.isHead = false;
+		this.isTail = false;*/
 		this.nextNode = null;
 		this.jumpToNode = null;
-	}	
+	}
+
+	public JumpListNode(){
+		this.nextNode = null;
+		this.jumpToNode = null;
+	}
 }
